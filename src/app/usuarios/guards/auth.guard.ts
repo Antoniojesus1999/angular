2f857ce186d2fe3065600457ec550e846ddf2e6c
INDRA\ajponce <ajponce@indra.es>
2022-03-23 15:40:13 +0100

    Implementación validación si expira el token

--- src/app/usuarios/guards/auth.guard.ts
+++ src/app/usuarios/guards/auth.guard.ts
@@ -1,3 +1,4 @@
+import { DATE_PIPE_DEFAULT_TIMEZONE } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -14,11 +15,27 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
       if (this.authService.isAuthenticated()) {
+
+        if (this.isTokenExpirado()) {
+          this.authService.logout();
+          this.router.navigate(['/login']);
+          return false;
+        }
         return true;
       }
       this.router.navigate(['/login'])
     return true;
   }
   
+  isTokenExpirado():boolean{
+    let token = this.authService.token;
+    let payload  =this.authService.obtenerDatosToken(token);
+    let now = new Date().getTime() / 1000;
+    if (payload.exp < now) {
+      return true;
+    }
+      return false;
+  }
 }
